d75e85e04d2a59576f33c9d1e97b46f0a8d67c08
Shabalin Dmitriy <shabalin.dmm@yandex.ru>
2025-01-03 21:06:22 +0500

    min-max assessments value and change gitignore for migrations

--- src/api/assessments/models.py
+++ src/api/assessments/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 from api.quizzes.models import Quiz
 
 
 class Assessment(models.Model):
     test = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='assessments')
-    min_pr = models.IntegerField()
-    max_pr = models.IntegerField()
+    min_pr = models.IntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(100)
+    ])
+    max_pr = models.IntegerField(validators=[
+            MinValueValidator(1),
+            MaxValueValidator(100)
+    ])
     name = models.CharField(max_length=20, verbose_name='Оценка')
     color = models.CharField(max_length=6, verbose_name='Цвет отображения')
