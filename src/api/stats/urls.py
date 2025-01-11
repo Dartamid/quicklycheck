856577070d8f0a9f20736c1b964428798a59a483
Dartamid <shabalin.dmm@yandex.ru>
2025-01-12 02:27:54 +0500

    new period

--- src/api/stats/urls.py
+++ src/api/stats/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from api.stats.views import GradeStatsByPeriodView
+from api.stats.views import GradeStatsByPeriodView, QuizGraphsView
 
 urlpatterns = [
-    path('stats/class/<int:grade_pk>/<str:period>/', GradeStatsByPeriodView.as_view(), name='grade_graph'),
+    path('stats/class/<int:grade_pk>/', GradeStatsByPeriodView.as_view(), name='grade_graph'),
+    path('stats/quiz/<int:quiz_pk>/', QuizGraphsView.as_view(), name='quiz_graph')
 ]
--- src/api/stats/views.py
+++ src/api/stats/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from api.grades.models import Grade
+from api.quizzes.models import Quiz
 from rest_framework.generics import get_object_or_404
 from api.teachers.permissions import IsTeacher
 from rest_framework.permissions import IsAuthenticated
@@ -7,10 +8,10 @@ from rest_framework import status
 from rest_framework.response import Response
 
 
-period_choices = {
-    'mouth': '%d.%m.%Y',
-    'year': '%m.%y'
-}
+period_choices = [
+    '%d.%m.%Y',
+    '%m.%y'
+]
 
 
 class GradeStatsByPeriodView(APIView):
@@ -25,13 +26,13 @@ class GradeStatsByPeriodView(APIView):
         self.check_object_permissions(self.request, obj)
         return obj
     
-    def get(self, request, grade_pk, period='mouth'):
+    def get(self, request, grade_pk):
         grade = self.get_object(request, grade_pk)
         blanks = grade.get_blanks()
-        if period in period_choices.keys():
-            per = period
+        if 'period' in request.query_params.keys():
+            per = request.query_params['period']
         else:
-            per = 'mouth'
+            per = 0
 
         raw_data = {}
 
@@ -52,4 +53,38 @@ class GradeStatsByPeriodView(APIView):
                 "avg": sum(raw_data[key]) / len(raw_data[key])
             })
 
+        return Response(clear_data, status=status.HTTP_200_OK)
+    
+
+class QuizGraphsView(APIView):
+    model = Quiz
+    permission_classes = (IsAuthenticated, IsTeacher)
+
+    def get_queryset(self, request):
+        return self.model.objects.filter(teacher=request.user).all()
+
+    def get_object(self, request, quiz_pk):
+        obj = get_object_or_404(self.get_queryset(request), pk=quiz_pk)
+        self.check_object_permissions(self.request, obj)
+        return obj
+    
+    def get(self, request, quiz_pk):
+        quiz = self.get_object(request, quiz_pk)
+        blanks = quiz.valid_blanks()
+
+        raw_data = {}
+        clear_data = {}
+        if len(blanks) > 0:
+            for blank in blanks:
+                for i, answer in enumerate(blank.score.checked_answers):
+                    if i in raw_data.keys():
+                        raw_data[str(i)] += 1 if answer['isRight'] else 0
+                    else:
+                        raw_data[str(i)] = 1 if answer['isRight'] else 0
+
+            most_hard = min(list(raw_data.items()), key=lambda x: x[1])
+            check = list(raw_data.values())
+            clear_data["questions"] = [item / len(blanks) * 100 for item in raw_data.values()],
+            clear_data["mostHard"] = most_hard[0]
+        
         return Response(clear_data, status=status.HTTP_200_OK)
