d5bd8df9b181fce7d5953e186a4476cb326711d8
Dartamid <shabalin.dmm@yandex.ru>
2025-01-13 00:18:21 +0500

    students graph

--- src/api/stats/views.py
+++ src/api/stats/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from api.grades.models import Grade
 from api.quizzes.models import Quiz
+from api.students.models import Student
 from rest_framework.generics import get_object_or_404
 from api.teachers.permissions import IsTeacher
 from rest_framework.permissions import IsAuthenticated
@@ -92,3 +93,46 @@ class QuizGraphsView(APIView):
             clear_data["mostHardQuestion"] = int(most_hard[0])
         
         return Response({"stats": clear_data}, status=status.HTTP_200_OK)
+    
+
+class StudentsGraphsView(APIView):
+    model = Student
+    permission_classes = (IsAuthenticated, IsTeacher)
+
+    def get_queryset(self, request):
+        return self.model.objects.filter(teacher=request.user).all()
+
+    def get_object(self, request, student_pk):
+        obj = get_object_or_404(self.get_queryset(request), pk=student_pk)
+        self.check_object_permissions(self.request, obj)
+        return obj
+    
+    def get(self, request, grade_pk):
+        student = self.get_object(request, grade_pk)
+        blanks = student.works.all()
+        if 'period' in request.query_params.keys():
+            per = int(request.query_params['period'])
+        else:
+            per = 0
+
+        raw_data = {}
+
+        for blank in blanks:
+            date = blank.created_at.strftime(period_choices[per])
+            if date in raw_data.keys():
+                raw_data[date].append(int(blank.score.percentage))
+            else:
+                raw_data[date] = [int(blank.score.percentage)]
+
+        clear_data = {
+            "stats": [],
+        }
+
+        for key in raw_data.keys():
+            clear_data['stats'].append({
+                "date": key,
+                "avg": sum(raw_data[key]) / len(raw_data[key])
+            })
+
+        return Response(clear_data, status=status.HTTP_200_OK)
+    
