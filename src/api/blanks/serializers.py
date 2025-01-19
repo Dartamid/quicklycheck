57f76ce6bfee17f4a86837e78da854a62cd8f3d2
Dartamid <shabalin.dmm@yandex.ru>
2025-01-20 01:08:41 +0500

    fix 2 assessments

--- src/api/blanks/serializers.py
+++ src/api/blanks/serializers.py
@@ -20,7 +20,7 @@ class BlankSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Blank
-        fields = ['pk', 'quiz', 'testName', 'author_info', 'image', 'id_blank', 'var', 'answers', 'blank_score', 'created_at']
+        fields = ['pk', 'quiz', 'assessment' ,'testName', 'author_info', 'image', 'id_blank', 'var', 'answers', 'blank_score', 'created_at']
         read_only_fields = ['image', 'author_info']
 
 
