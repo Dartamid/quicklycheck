c33e214c869322d3d7a4abaf3cd782f698ab9f26
Dartamid <shabalin.dmm@yandex.ru>
2025-01-21 22:13:17 +0500

    temps fixes

--- src/api/temps/models.py
+++ src/api/temps/models.py
@@ -14,8 +14,41 @@ def json_checked_answers():
         'isRight': False,
     } for _ in range(40)]
 
+def json_assessments():
+    default = [
+        {
+            'name': '2',
+            'minPr': 0,
+            'maxPr': 40,
+            'color': '#ff0000',
+        },
+        {
+            'name': '3',
+            'minPr': 40,
+            'maxPr': 60,
+            'color': '#ff0000',
+        },
+        {
+            'name': '4',
+            'minPr': 60,
+            'maxPr': 80,
+            'color': '#ff0000',
+        },
+        {
+            'name': '5',
+            'minPr': 80,
+            'maxPr': 100,
+            'color': '#ff0000',
+        }
+    ]
+    return default
+
 
 class TempQuiz(models.Model):
+    assessments = models.JSONField(
+        'Градация оценок',
+        default=json_assessments,
+    )
     created_at = models.DateTimeField(
         verbose_name='Дата создания',
         auto_now_add=True
@@ -64,6 +97,15 @@ class TempBlank(models.Model):
         verbose_name='Дата создания',
         auto_now_add=True
     )
+
+    def set_assessment(self):
+        if self.score.is_checked:
+            assessments = self.quiz.assessments
+            per = self.score.percentage
+            for assessment in assessments:
+                if assessment['min_pr'] <= per <= assessment['max_pr'] or per == assessment['min_pr'] == 0:
+                    self.assessment = assessment['name']
+                    self.save()
     
     
 class TempScore(models.Model):
